c15647fc81a84118a0c3972f4dbd0114aef5184e
milenasvieira <milenasvieira63@gmail.com>
2021-08-04 22:35:14 -0300

    first django api

---
+++ library/books/api/viewsets.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from books.api import serializers
+from books import models
+
+class BooksViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.BooksSerializer
+    queryset = models.Books.objects.all() #Todos os objetos que estão dentro do model Books
+    
